f085a45a401b13e4249b2d756229aa1812eea1e0
Thole Goesmann <thole.goesmann@gmx.de>
2021-02-04 18:56:30 +0100

    Zustand nach dem zweiten Treffen

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class TicTacToe {
   public static void main(String[] args) {
 		char[][] field = createField();
-		printField(field);
 		boolean playerOne = getFirstPlayer();
-		System.out.println(playerOne);
+		Scanner scanner = new Scanner(System.in);
+		int xCoordinate, yCoordinate;
+		
+		printField(field);
+		
+		while (hasEmptyField(field) && !hasPlayerWon(field)) {
+      yCoordinate = getCoordinate(scanner);
+		  xCoordinate = getCoordinate(scanner);
+      // richtige Eingabe erzwingen
+      while (!isValid(xCoordinate, yCoordinate, field)) {
+        System.out.println("Das Feld ist außerhalb des Spielfelds oder bereits belegt.");
+        yCoordinate = getCoordinate(scanner);
+		    xCoordinate = getCoordinate(scanner);
+      } 
+      
+      if (playerOne) {
+        field[xCoordinate][yCoordinate] = 'X';
+        playerOne = false;
+      } else {
+      	field[xCoordinate][yCoordinate] = 'O';
+        playerOne = true;
+    	}
+    	printField(field);
+		}
   }
   
   public static char[][] createField() {
@@ -21,7 +46,7 @@ public class TicTacToe {
     
     // Ausgabe der Zahlen für die Spalten
     System.out.print(" ");
-    for (int y = 1; y <= field[0].length; y++) {
+    for (int y = 0; y < field[0].length; y++) {
     	strich += "--"; // Länge des honrizontalen Strichs bestimmen
     	System.out.print(" " + y);
   	}
@@ -45,5 +70,54 @@ public class TicTacToe {
   	} else {
   	  return false;
   	}
+  }
+  
+  public static boolean isValid(int x, int y, char[][] field) {
+    return x >= 0 && x < field.length && y >= 0 && y < field[x].length
+        && field[x][y] == ' ';
+  }
+  
+  public static boolean hasEmptyField(char[][] field) {
+    for (int x = 0; x < field.length; x++) {
+      for (int y = 0; y < field[x].length; y++) {
+        if (field[x][y] == ' ') {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  
+  public static boolean hasPlayerWon(char[][] field) {
+    for (int x = 0; x < field.length; x++) {
+      if (field[x][0] != ' ' && field[x][0] == field[x][1] && field[x][1] == field[x][2]) {
+        return true;
+      }
+    }
+    for (int y = 0; y < field[0].length; y++) {
+      if (field[0][y] != ' ' && field[0][y] == field[1][y] && field[1][y] == field[2][y]) {
+        return true;
+      }
+    }
+    if (field[0][0] != ' ' && field[0][0] == field[1][1] && field[1][1] == field[2][2]) {
+     return true;
+    }
+    if (field[0][2] != ' ' && field[0][2] == field[1][1] && field[1][1] == field[2][0]) {
+     return true;
+    }
+    return false;
+  }
+  
+  public static int getCoordinate(Scanner scanner) {
+    int coordinate;
+    try {
+      coordinate = scanner.nextInt();
+    } catch (InputMismatchException ex) {
+      System.out.println("Bitte gebe eine Zahl ein.");
+      // token aus scanner entfernen
+      scanner.next();
+      coordinate = getCoordinate(scanner);    
+    }
+    return coordinate;
   }
 }
